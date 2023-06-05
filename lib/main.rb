e39848259b4504835e2108776b036d34b6e5df03
Paul Martin <paulmartinblopez@gmail.com>
2023-06-05 14:55:42 +0800

    Add delete node
    * insert node will now account for duplicate inserted value

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,6 @@
 class Node
   include Comparable
-  attr_accessor :left_child, :right_child
-  attr_reader :data
+  attr_accessor :left_child, :right_child, :data
 
   def initialize(value)
     @data = value
@@ -32,11 +31,26 @@ class Tree
   # Wonder how others did this, mine is a bit questionable because of re-assign of already correct nodes
   def insert(value, current_node = @root)
     return Node.new(value) if current_node.nil?
+    return current_node if current_node.data == value
 
     if value < current_node.data
       current_node.left_child = insert(value, current_node.left_child)
     else
       current_node.right_child = insert(value, current_node.right_child)
+    end
+    current_node
+  end
+
+  def delete(value, current_node = @root)
+    return nil if current_node.nil?
+
+    result = value <=> current_node.data
+    if result.negative?
+      current_node.left_child = delete(value, current_node.left_child)
+    elsif result.positive?
+      current_node.right_child = delete(value, current_node.right_child)
+    else
+      return delete_matched(current_node)
     end
     current_node
   end
@@ -47,13 +61,35 @@ class Tree
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
   end
+
+  private
+
+  def delete_matched(delete_node)
+    if delete_node.left_child.nil?
+      delete_node.right_child
+    elsif delete_node.right_child.nil?
+      delete_node.left_child
+    else
+      lowest_node = find_min_value_node(delete_node.right_child)
+      delete_node.data = lowest_node.data
+      delete_node.right_child = delete(lowest_node.data, delete_node.right_child)
+      delete_node
+    end
+  end
+
+  def find_min_value_node(right_subtree)
+    lowest_node = right_subtree
+    lowest_node = lowest_node.left_child until lowest_node.left_child.nil?
+    lowest_node
+  end
 end
 
 test_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 quack = Tree.new(test_array)
-quack.insert(123)
-quack.insert(12)
-quack.insert(10)
-quack.insert(13)
-quack.insert(14)
+quack.insert(24)
+quack.insert(25)
+quack.insert(26)
+quack.pretty_print
+puts '-----------------------------'
+quack.delete(24)
 quack.pretty_print
